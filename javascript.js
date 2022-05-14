9448c33857118c877f9c8d448d7c9277eadef29b
Aliya Gamez <aliya.e.gamez@gmail.com>
2022-05-13 21:07:47 -0400

    practice

---
+++ javascript.js
@@ -0,0 +1,39 @@
+/*
+
+let message; //declares a variable
+message = 'Hello';
+
+//OR let message = 'Hello';
+
+alert(message);
+
+let user = 'John', age = 25, msg = 'Hi im john';
+
+//var variable; is the "old way" of declaring variables
+
+const myBday = '21.06.2001';
+//this variable cannot be changed!
+//it is standard to use CAPITAL LETTERS for consts
+const COLOR_RED = '#F00';
+
+//Tasks
+let admin,
+    name;
+
+name = 'John';
+admin = name;
+
+alert(admin); */
+
+//Practice
+
+console.log('23 + 97 = ' + (23 + 97));
+console.log('(4 + 6 + 9) / 77 = ' + ((4 + 6 + 9) / 77));
+
+let a = 10;
+console.log('a = ' + a);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log('percentage = ' + percentage);
